dc2d232ae007b191caf3a56af1cbe27adb2924be
mhz <mhzintb@gmail.com>
2025-10-06 07:03:51 -0300

    refactor: remove old commands and interactions

--- src/bot/events/ready.ts
+++ src/bot/events/ready.ts
@@ -1,4 +1,6 @@
-import { Event, type ExtendedClient } from "../../core";
+import { REST, Routes } from "discord.js";
+
+import { Event, ExtendedClient } from "../../core";
 
 import { logger } from "../../utils/logger";
 
@@ -7,7 +9,23 @@ class ClientReadyEvent extends Event<"clientReady"> {
   once = true;
 
   async execute(client: ExtendedClient): Promise<void> {
-    logger.info(`Conectado como ${client.user?.tag}`);
+    try {
+      logger.info("[*] Registrando comandos via REST...");
+
+      const rest = new REST({ version: "10" }).setToken(process.env.TOKEN!);
+
+      if (client.commands.size === 0) {
+        logger.warn("[!] Nenhum comando carregado para registrar");
+      } else {
+        const body = client.commands.map((cmd) => cmd.data.toJSON());
+        await rest.put(Routes.applicationCommands(client.user!.id), { body });
+        logger.info(`[+] Comandos registrados: ${client.commands.size}`);
+      }
+    } catch (error: any) {
+      logger.error("[!] Erro ao registrar comandos:", error);
+    }
+
+    logger.info(`[+] Conectado como ${client.user?.tag}`);
   }
 }
 
